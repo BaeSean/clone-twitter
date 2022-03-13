4d775dfe0a12e9a8dafc6cd953b27544fb21e1bd
Seungyong Bae <seungyongbae@Seungyongui-MacBookAir.local>
2022-03-13 13:29:37 +0900

    Make key file(.env)

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnU2p-8sAqiUm2J0DwovWX-VLkXQOkfck",
-  authDomain: "clone-twitter-25f0d.firebaseapp.com",
-  projectId: "clone-twitter-25f0d",
-  storageBucket: "clone-twitter-25f0d.appspot.com",
-  messagingSenderId: "628726588514",
-  appId: "1:628726588514:web:3db72d6a5e1e4b3503dd3b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
